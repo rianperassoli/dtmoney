249462f66416c83cc2b71a1001d99fb9341b6461
Rian Perassoli <rian.perassoli@gmail.com>
2021-06-09 19:56:16 -0300

    Adicionado Componente de MODAL ao projeto e configurado

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import { Dashboard } from './components/Dashboard';
+import { useState } from 'react'
+import Modal from 'react-modal'
+import { Dashboard } from './components/Dashboard'
 import { Header } from './components/Header'
-import { GlobalStyle } from "./styles/global";
+
+import { GlobalStyle } from "./styles/global"
+
+Modal.setAppElement('#root')
 
 export function App() {
+
+  const [isNewTransactionModalOpen, SetIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    SetIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    SetIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   )
