1aef1f93b0c1aaa16cbcac5a8a4752d6f2256c02
Rian Perassoli <rian.perassoli@gmail.com>
2021-06-10 09:38:14 -0300

    Adicionado polished ao projeto e adicionados botoes de entrada e saída ao modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,9 @@
 import Modal from 'react-modal'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
 
-import { Container } from './styles'
+import { Container, TransactionTypeContainer } from './styles'
 
 Modal.setAppElement('#root')
 
@@ -30,10 +32,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       <Container>
         <h2>Cadastrar transação</h2>
 
-
         <input placeholder="Título" />
 
         <input type="number" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
 
         <input placeholder="Categoria" />
 
