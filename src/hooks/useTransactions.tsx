18deff0061bd18b7aab51cbc4a5de27993d56f69
Rian Perassoli <rian.perassoli@gmail.com>
2021-06-15 21:28:37 -0300

    Criado hook do Transaction Context

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, useEffect, useState, ReactNode } from 'react'
-import { api } from './services/api'
+import { createContext, useEffect, useState, ReactNode, useContext } from 'react'
+import { api } from '../services/api'
 
 interface Transaction {
   id: number
@@ -21,7 +21,7 @@ interface TransactionsContextData {
   createTransaction: (transactionInput: TransactionInput) => Promise<void>
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData)
+const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData)
 
 export const TransactionsProvider = ({ children }: TransactionsProviderProps) => {
   const [transactions, setTransactions] = useState<Transaction[]>([])
@@ -44,4 +44,10 @@ export const TransactionsProvider = ({ children }: TransactionsProviderProps) =>
       { children}
     </TransactionsContext.Provider>
   )
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext)
+
+  return context
 }
